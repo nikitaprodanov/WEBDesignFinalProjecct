65caf31830ecfd34fe11d7e173e6781fc69c11eb
Nikita <uchihanaruto9371@gmail.com>
2020-05-09 14:13:25 +0300

    Added sass to css compiler with gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// Compile scss into css
+function style() {
+    // where is my scss file
+    return gulp.src('./sass/**/*.sass')
+    // pass that file through sass compiler
+    .pipe(sass())
+    //  where do I save the compiled CSS
+    .pipe(gulp.dest('./css'))
+}
+
+exports.style = style;
