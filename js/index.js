d59b8e8360f4d28f9666f7a42c1cc1a63a73211c
Nikita <uchihanaruto9371@gmail.com>
2020-05-20 19:26:01 +0300

    Drop down menu

--- js/index.js
+++ js/index.js
@@ -7,4 +7,38 @@ const navSlide = () => {
     });
 }
 
-navSlide();
+const dropdownPlansDesk = () => {
+    const plans = document.querySelector('.plans-link');
+    const plansDropDesk = document.querySelector('.dropdown-plans');
+
+    plans.addEventListener('click', () => {
+        plansDropDesk.classList.toggle('dropdown-plans-active');
+    });
+}
+
+const dropdownTrainingsDesk = () => {
+    const trainings = document.querySelector('.trainings-link');
+    const trainingsDropDesk = document.querySelector('.dropdown-training');
+
+    trainings.addEventListener('click', () => {
+        trainingsDropDesk.classList.toggle('dropdown-training-active');
+    });
+}
+
+const dropdownNutritionsDesk = () => {
+    const nutrition = document.querySelector('.nutrition-link');
+    const nutritionsDropDesk = document.querySelector('.dropdown-nutrition');
+
+    nutrition.addEventListener('click', () => {
+        nutritionsDropDesk.classList.toggle('dropdown-nutrition-active');
+    });
+}
+
+const index = () => {
+    navSlide();
+    dropdownPlansDesk();
+    dropdownTrainingsDesk();
+    dropdownNutritionsDesk();
+}
+
+index();
